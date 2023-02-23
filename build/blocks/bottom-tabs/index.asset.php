1fc6fe59fdd2dc8bfc7f0fd615035a43fd0775d0
naumanahmed19 <naumanahmed19@gmail.com>
2023-02-23 19:05:28 +0000

    Build: (b8bc13d) Merge pull request #13 from naumanahmed19/feat/core-blocks
    chore: added console

--- build/blocks/bottom-tabs/index.asset.php
+++ build/blocks/bottom-tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2819255a2c1cde546171');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '047a4e4ae650e3c9f36c');
