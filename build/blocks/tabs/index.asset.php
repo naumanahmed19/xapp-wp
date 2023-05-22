e025b0a250899dd00075d90f32dbcdf3a452be2d
naumanahmed19 <naumanahmed19@gmail.com>
2023-05-22 08:03:38 +0000

    Build: (ff19d79) fix: colors control to add theme colors

--- build/blocks/tabs/index.asset.php
+++ build/blocks/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2aaa454dc9de9d9e3ef2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e3acf47b5194b8e4bc44');
--- build/non-block-examples/meta/index.asset.php
+++ build/non-block-examples/meta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '210f47c20236ef30fe38');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'c9007ea5b909b2917712');
