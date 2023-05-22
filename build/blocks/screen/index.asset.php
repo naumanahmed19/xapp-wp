070de15221c5df9d057734c5b4017d302df5c878
naumanahmed19 <naumanahmed19@gmail.com>
2023-05-22 10:25:59 +0000

    Build: (344aab3) fix: home screen json fetch

--- build/blocks/screen/index.asset.php
+++ build/blocks/screen/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ffa3cd61f0623061b963');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5034dbf94c8a613fe06d');
