5a81f200c697e9acd44d8b9e1902b321ec756776
naumanahmed19 <naumanahmed19@gmail.com>
2023-05-25 14:50:21 +0000

    Build: (f60172c) fix: language-switcher

---
+++ build/blocks/language-switcher/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '96dc2c2ad0ad4f7f9290');
--- build/blocks/locales/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => 'ef6096b4703582f4aa62');
