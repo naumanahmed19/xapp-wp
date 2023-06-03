be2c57392ec2ec8f1ffa765196fc88d8ed5909f4
naumanahmed19 <naumanahmed19@gmail.com>
2023-06-03 00:36:25 +0000

    Build: (767c9bb) fix: __experimentalLabel

--- build/blocks/text/index.asset.php
+++ build/blocks/text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => 'df2e4d9e69d1ba89348a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => 'd0529b32d0e466735023');
