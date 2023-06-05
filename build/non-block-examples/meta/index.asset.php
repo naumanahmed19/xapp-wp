e4a397aa2e7a9313d6e6d45a2dff7c845dabdc45
naumanahmed19 <naumanahmed19@gmail.com>
2023-06-05 14:02:52 +0000

    Build: (f4d0dda) fix: app settings panel

--- build/non-block-examples/meta/index.asset.php
+++ build/non-block-examples/meta/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => '2a8ab8978cbc6fcc3c97');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'f43c7c46681fd27ccc3e');
