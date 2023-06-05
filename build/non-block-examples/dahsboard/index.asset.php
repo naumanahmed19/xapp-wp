9b6df5d510474e1c1eedb9eb7a46a95136c3fd30
naumanahmed19 <naumanahmed19@gmail.com>
2023-06-05 12:55:40 +0000

    Build: (43deb09) fix: locale

--- build/non-block-examples/dahsboard/index.asset.php
+++ build/non-block-examples/dahsboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b4a1b04deab47eef27a2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c995801d0e79fed96d05');
