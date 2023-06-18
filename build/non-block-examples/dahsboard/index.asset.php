2bc621d69f0458a417c07214c4091d93da36c248
naumanahmed19 <naumanahmed19@gmail.com>
2023-06-18 22:20:29 +0000

    Build: (5f3165c) fix: downloads

--- build/non-block-examples/dahsboard/index.asset.php
+++ build/non-block-examples/dahsboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a950db8e0e51cfcdfad2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b970da18c1fb2a282d85');
