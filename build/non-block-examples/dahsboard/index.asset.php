37f2aff42b1997ff037510b20743c4ceddc5e9cb
naumanahmed19 <naumanahmed19@gmail.com>
2023-06-30 13:38:36 +0000

    Build: (756c805) fix: edit url on multisite

--- build/non-block-examples/dahsboard/index.asset.php
+++ build/non-block-examples/dahsboard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '85688557128108e2fdbe');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '801f0d4fc210de65e38d');
