1ed23c93514fd4e6424756dd4e7aa76c54f89841
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-01 11:12:57 +0000

    Build: (7573284) fix: disable border radius

--- build/blocks/tabs/index.asset.php
+++ build/blocks/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '98b1ed92dbabb38a4e54');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd8de13a49ef4fad74396');
