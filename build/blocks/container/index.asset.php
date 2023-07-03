dc25af7038ba3972bb4fc408e224ebf7a17d5515
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-03 14:51:30 +0000

    Build: (91f415c) fix: layout

--- build/blocks/container/index.asset.php
+++ build/blocks/container/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ceb6fa0e6508e73dd976');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7b93fc03cdce385a44d5');
