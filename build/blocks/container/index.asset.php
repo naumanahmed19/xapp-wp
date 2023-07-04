92ffbb32ac6c8404b8cf0c11fd778ce1735fbc97
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-04 00:26:22 +0000

    Build: (74496ed) fix: container aligment

--- build/blocks/container/index.asset.php
+++ build/blocks/container/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7b93fc03cdce385a44d5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f572871622c440326f0c');
--- xapp.php
+++ xapp.php
@@ -164,24 +164,7 @@ function xapp_post_rest_payload($data, $post, $context)
 }
 add_filter('rest_prepare_post', 'xapp_post_rest_payload', 10, 3);
 add_filter('rest_prepare_product', 'xapp_post_rest_payload', 10, 3);
-add_action('rest_api_init', 'register_rest_images' );
-function register_rest_images(){
-    register_rest_field( array('post'),
-        'fimg_url',
-        array(
-            'get_callback'    => 'get_rest_featured_image',
-            'update_callback' => null,
-            'schema'          => null,
-        )
-    );
-}
-function get_rest_featured_image( $object, $field_name, $request ) {
-    if( $object['featured_media'] ){
-        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
-        return $img[0];
-    }
-    return false;
-}
+
 
 
 /**
