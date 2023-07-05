e02def3429191e25c56e85f984ee3b767972fcb8
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-05 22:37:08 +0000

    Build: (9c0f053) fix: icon

--- build/blocks/icon/index.asset.php
+++ build/blocks/icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => 'ab4c8fd9286dd5329f93');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '54f7a006af131e7a3809');
