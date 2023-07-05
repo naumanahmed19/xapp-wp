330606933677d4ec2a3d149254626889d77da4b4
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-05 23:57:47 +0000

    Build: (d23d6f1) fix: icon backup

--- build/blocks/icon/index.asset.php
+++ build/blocks/icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '6391b009daf2c7179df2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '045083ffba4839767e3d');
