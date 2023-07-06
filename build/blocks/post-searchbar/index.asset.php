06bf498d4287d446e4d3be0a44947faeced55839
naumanahmed19 <naumanahmed19@gmail.com>
2023-07-06 00:56:21 +0000

    Build: (07f7309) fix: search

--- build/blocks/post-searchbar/index.asset.php
+++ build/blocks/post-searchbar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '939df5683882010a79a9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => 'e287c78223512dee43f7');
