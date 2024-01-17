266be1e344c38589a495ce908103dceba5699ce9
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-17 16:09:15 +0000

    Build: (d16f851) Add flexible appbar as parent in safe area block.json

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "ccaf98f02a3443df5931c32d70139a3e",
-"index.html": "83497e9db3b0088fc29f6c4238d829f5",
-"/": "83497e9db3b0088fc29f6c4238d829f5",
+"index.html": "14169403da25d6790008a65b6b1331ec",
+"/": "14169403da25d6790008a65b6b1331ec",
 "assets/AssetManifest.bin": "74418c1756988de9a7b4a53e54972d0b",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/images/g.svg": "f26d3f11fbc045c3bc3b281fb2075b02",
