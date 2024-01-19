54b8014958662c8fea04a97dc39751231c1839a2
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-19 23:17:13 +0000

    Build: (373d0a0) fix: appbar

--- build/blocks/appbar/index.asset.php
+++ build/blocks/appbar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1b6636af85d4ba39742f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd3130ba4a8e744a666e6');
