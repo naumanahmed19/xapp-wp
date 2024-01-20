27297fbc20d44deb54eb5246bc4141e6034b6e73
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-20 01:59:41 +0000

    Build: (f458080) Remove unsupported "multiple" property from block.json

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "ccaf98f02a3443df5931c32d70139a3e",
-"index.html": "bab47ff4da11ecf9eb5939ee0cbe10ad",
-"/": "bab47ff4da11ecf9eb5939ee0cbe10ad",
+"index.html": "c423be06bfc4e15828b37b302ac216e0",
+"/": "c423be06bfc4e15828b37b302ac216e0",
 "assets/AssetManifest.bin": "74418c1756988de9a7b4a53e54972d0b",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/images/g.svg": "f26d3f11fbc045c3bc3b281fb2075b02",
