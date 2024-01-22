f2a88d1da8641430a8e35195dd0b6f2ae2abe252
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-22 23:10:36 +0000

    Build: (d1c1a5e) Remove unused "settings" object from block.json and edit.js

--- build/blocks/text/index.asset.php
+++ build/blocks/text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '99bea043afebc733421c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '574a6b8fe38d4c2138b2');
