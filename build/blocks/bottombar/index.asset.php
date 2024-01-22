f28b9fbbeebad671c5d04b4c7de17320a4ae1df7
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-22 20:56:08 +0000

    Build: (6dcfba5) Update editor.scss and edit.js files

--- build/blocks/bottombar/index.asset.php
+++ build/blocks/bottombar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '245ee5a9714ca45159bd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ca8791c711fcab1e9af3');
