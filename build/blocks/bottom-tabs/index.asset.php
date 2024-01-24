641f4711536304087c8f1eb1c12b62a6d76cf18a
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-24 06:56:54 +0000

    Build: (8d5eb2d) Fix useEffect dependency in bottom-tabs/edit.js

--- build/blocks/bottom-tabs/index.asset.php
+++ build/blocks/bottom-tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '08f804a14ee16ad73549');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6448f557ec64d999ed51');
