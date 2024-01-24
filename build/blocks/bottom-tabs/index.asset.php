4cc6181ec00d6c448d335a175671b314a9889d26
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-24 17:34:42 +0000

    Build: (fe8b25e) Update tabBar function parameter

--- build/blocks/bottom-tabs/index.asset.php
+++ build/blocks/bottom-tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8ecbd50d7c25def50429');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b9c5edfa7ed1ef89d56f');
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "ccaf98f02a3443df5931c32d70139a3e",
-"index.html": "ae98f377b06ac3e377ac4d2bf1e6cb33",
-"/": "ae98f377b06ac3e377ac4d2bf1e6cb33",
+"index.html": "c83e0d25a97d0b529759afeb0afff783",
+"/": "c83e0d25a97d0b529759afeb0afff783",
 "assets/AssetManifest.bin": "74418c1756988de9a7b4a53e54972d0b",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/images/g.svg": "f26d3f11fbc045c3bc3b281fb2075b02",
