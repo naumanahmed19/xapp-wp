8567848d11b21d4d0c48b07b4c8ed10e8dc3a107
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-28 00:24:56 +0000

    Build: (71d95ef) Refactor button block edit.js and index.js

--- build/blocks/button/index.asset.php
+++ build/blocks/button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '39f0eedb885620f92cf1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'e7aa0e6adceea4a91159');
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -71,8 +71,8 @@ const RESOURCES = {"main.dart.js": "707698a1ad482e2be06711f24db70b6c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3a741226093b436ed9af5b655e7e5e75",
-"/": "3a741226093b436ed9af5b655e7e5e75",
+"index.html": "30ca4979f9f5c1a2e7f3e1c95d05da2b",
+"/": "30ca4979f9f5c1a2e7f3e1c95d05da2b",
 "version.json": "3183ca888b8b86c7efa411e367c8cba5",
 "manifest.json": "ccaf98f02a3443df5931c32d70139a3e"};
 // The application shell files that are downloaded before a service worker can
