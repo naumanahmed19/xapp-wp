7fd47b605728c636410e52479b9b9553d0f9bfb4
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-28 00:00:15 +0000

    Build: (6cf725d) Refactor button edit.js to use destructuring and remove unused code

--- build/blocks/button/index.asset.php
+++ build/blocks/button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'a47b04cdab10e4f57291');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '39f0eedb885620f92cf1');
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -71,8 +71,8 @@ const RESOURCES = {"main.dart.js": "707698a1ad482e2be06711f24db70b6c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f9e92f860719de5b6ea148ffdbd3b686",
-"/": "f9e92f860719de5b6ea148ffdbd3b686",
+"index.html": "3a741226093b436ed9af5b655e7e5e75",
+"/": "3a741226093b436ed9af5b655e7e5e75",
 "version.json": "3183ca888b8b86c7efa411e367c8cba5",
 "manifest.json": "ccaf98f02a3443df5931c32d70139a3e"};
 // The application shell files that are downloaded before a service worker can
