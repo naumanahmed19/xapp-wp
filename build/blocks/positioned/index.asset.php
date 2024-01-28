2ec1175e72d14cb151899b3a6e00840e8a33d0d1
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-28 23:52:59 +0000

    Build: (e4851fd) Refactor positioned block edit.js and index.js

--- build/blocks/positioned/index.asset.php
+++ build/blocks/positioned/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '35103f4a59d2e21fdb8a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '55ccfef9d43a789bf5ff');
