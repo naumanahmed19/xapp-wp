93c400bfeae4971127dede216f473fce86a799a9
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-29 23:25:22 +0000

    Build: (4012c90) Fix linkTo value in InspectorControlsButton.js

--- build/blocks/button/index.asset.php
+++ build/blocks/button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '7c09ec3a3ef47dd090b1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '9af9d9fe8787592c4807');
--- build/blocks/icon-button/index.asset.php
+++ build/blocks/icon-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '5cfa5d513418dfb178fc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '44050ed47fd67024b906');
