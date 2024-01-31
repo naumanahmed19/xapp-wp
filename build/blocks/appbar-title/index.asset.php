4d08748973526eeef712e5787ee45d8a877ed9a5
naumanahmed19 <naumanahmed19@gmail.com>
2024-01-31 15:53:13 +0000

    Build: (dab82cd) Add CSS styles for appbar title

--- build/blocks/appbar-title/index.asset.php
+++ build/blocks/appbar-title/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '968447c8a53288ed3e52');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'b22c7148c415d4fc7f27');
